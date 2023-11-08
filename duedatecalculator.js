34d8dea5da1709dc72292e8662349a3840c8dcce
gFarkas1981 <info@gfarkas.com>
2023-11-08 13:55:46 +0100

    Pass test for throwing error for turnaroundTime type

--- duedatecalculator.js
+++ duedatecalculator.js
@@ -2,6 +2,9 @@ function calculateDueDate(submitDate, turnaroundTime) {
     if (!(isValidDate(submitDate))) {
         throw ('Type of submit date must be a date!')
     }
+    if (!Number.isInteger(turnaroundTime)) {
+        throw ('Type of turnaroundTime must be an integer!')
+    }
     const dueDate = new Date('2023-11-10T08:00:00');
 
     return dueDate;

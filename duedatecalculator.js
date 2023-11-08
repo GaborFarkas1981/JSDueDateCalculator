f1bfb03f429571e312ba6909fd2463504ebb1ba5
gFarkas1981 <info@gfarkas.com>
2023-11-08 12:02:06 +0100

    Failed test for throwing error for submitDate type

--- duedatecalculator.js
+++ duedatecalculator.js
@@ -1,4 +1,7 @@
 function calculateDueDate(submitDate, turnaroundTime) {
 
+    const dueDate = new Date('2023-11-10T08:00:00');
+
+    return dueDate;
 }
 module.exports = calculateDueDate;
--- duedatecalculator.test.js
+++ duedatecalculator.test.js
@@ -3,9 +3,20 @@ expect.extend({ toBeDateString });
 
 const calculateDueDate = require('./duedatecalculator');
 
-it('should return dueDate in date format', () => {
-    const submitDate = new Date('2023-11-08T10:11:12');
-    const turnaroundTime = 8;
-    const result = calculateDueDate(submitDate, turnaroundTime);
-    expect(result).toBeDateString();
-  });
+describe('calculateDueDate', () => {
+    it('should return dueDate in date format', () => {
+        const submitDate = new Date('2023-11-08T10:11:12');
+        const turnaroundTime = 8;
+        const result = calculateDueDate(submitDate, turnaroundTime);
+        expect(result).toBeDateString();
+      });
+
+    it('should throw an error if submit date is not a string', () => {
+      const submitDate = 33;
+      const turnaroundTime = 8;
+      const t = () => {
+        calculateDueDate(submitDate, turnaroundTime);
+      };
+      expect(t).toThrow('Format of submit date must be a string!');
+    });
+});

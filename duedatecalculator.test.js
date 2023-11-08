d650b0f56ae8fa5006d4573704d99993b2b6b986
gFarkas1981 <info@gfarkas.com>
2023-11-08 13:51:24 +0100

    Failed test for throwing error for turnaroundTime type

--- duedatecalculator.test.js
+++ duedatecalculator.test.js
@@ -13,6 +13,16 @@ describe('submitDate type check', () => {
       expect(t).toThrow('Type of submit date must be a date!');
     });
 });
+describe('turnaroundTime type check', () => {
+    it('should throw an error if type of turnaroundTime is not a number', () => {
+      const submitDate = new Date('2023-11-08T10:11:12');
+      const turnaroundTime = 8.8;
+      const t = () => {
+        calculateDueDate(submitDate, turnaroundTime);
+      };
+      expect(t).toThrow('Type of turnaroundTime must be an integer!');
+    });
+});
 describe('return type check', () => {
     it('should return dueDate in date format', () => {
         const submitDate = new Date('2023-11-08T10:11:12');

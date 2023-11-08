30dfa6b7ad3fee05bced34de9339f9095036f91f
gFarkas1981 <info@gfarkas.com>
2023-11-08 14:32:02 +0100

    Refactoring

--- duedatecalculator.test.js
+++ duedatecalculator.test.js
@@ -3,7 +3,7 @@ expect.extend({ toBeDateString });
 
 const calculateDueDate = require('./duedatecalculator');
 
-describe('submitDate type check', () => {
+describe('type check', () => {
     it('should throw an error if type of submitDate is not date', () => {
       const submitDate = 33;
       const turnaroundTime = 8;
@@ -12,8 +12,6 @@ describe('submitDate type check', () => {
       };
       expect(t).toThrow('Type of submit date must be a date!');
     });
-});
-describe('turnaroundTime type check', () => {
     it('should throw an error if type of turnaroundTime is not a number', () => {
       const submitDate = new Date('2023-11-08T10:11:12');
       const turnaroundTime = 'test';
@@ -22,12 +20,10 @@ describe('turnaroundTime type check', () => {
       };
       expect(t).toThrow('Type of turnaroundTime must be an integer!');
     });
-});
-describe('return type check', () => {
     it('should return dueDate in date format', () => {
         const submitDate = new Date('2023-11-08T10:11:12');
         const turnaroundTime = 8;
         const result = calculateDueDate(submitDate, turnaroundTime);
         expect(result).toBeDateString();
-      });
+    });
 });

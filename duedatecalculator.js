aac6a35561193609b2f90ff27a823fdb8e83a0ae
gFarkas1981 <info@gfarkas.com>
2023-11-08 13:23:48 +0100

    Passed test for throwing error for submitDate type

--- duedatecalculator.js
+++ duedatecalculator.js
@@ -1,5 +1,7 @@
 function calculateDueDate(submitDate, turnaroundTime) {
-
+    if (!(submitDate instanceof Date && !isNaN(submitDate))) {
+        throw ('Format of submit date must be a date!')
+    }
     const dueDate = new Date('2023-11-10T08:00:00');
 
     return dueDate;
--- duedatecalculator.test.js
+++ duedatecalculator.test.js
@@ -3,20 +3,21 @@ expect.extend({ toBeDateString });
 
 const calculateDueDate = require('./duedatecalculator');
 
-describe('calculateDueDate', () => {
+describe('submit date type check', () => {
+    it('should throw an error if format of submit date is not date', () => {
+      const submitDate = 33;
+      const turnaroundTime = 8;
+      const t = () => {
+        calculateDueDate(submitDate, turnaroundTime);
+      };
+      expect(t).toThrow('Format of submit date must be a date!');
+    });
+});
+describe('return type check', () => {
     it('should return dueDate in date format', () => {
         const submitDate = new Date('2023-11-08T10:11:12');
         const turnaroundTime = 8;
         const result = calculateDueDate(submitDate, turnaroundTime);
         expect(result).toBeDateString();
       });
-
-    it('should throw an error if submit date is not a string', () => {
-      const submitDate = 33;
-      const turnaroundTime = 8;
-      const t = () => {
-        calculateDueDate(submitDate, turnaroundTime);
-      };
-      expect(t).toThrow('Format of submit date must be a string!');
-    });
 });

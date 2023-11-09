11b129b57cff0b1c897934139d3a8d4cf63e6442
gFarkas1981 <info@gfarkas.com>
2023-11-09 11:22:13 +0100

    Pass test for submitDate not in working hours

--- duedatecalculator.js
+++ duedatecalculator.js
@@ -1,4 +1,6 @@
 function calculateDueDate(submitDate, turnaroundTime) {
+    const dstOffset = getLocalDSTOffset(); // considering DST offset
+    submitDate = new Date(submitDate.getTime() + dstOffset * 60000);
     inputValidator(submitDate, turnaroundTime);
     const dueDate = new Date('2023-11-10T08:00:00');
 
@@ -13,9 +15,38 @@ function inputValidator(submitDate, turnaroundTime) {
     if (!(isValidDate(submitDate))) {
         throw ('Type of submitDate must be a date!')
     }
+    if (!isWorkingHour(submitDate)) {
+        throw ('Submit date must be in working hours!')
+    }
     if (!Number.isInteger(turnaroundTime)) {
         throw ('Type of turnaroundTime must be an integer!')
     }
 }
 
+function isWorkingDay(date) {
+  const workingDays = [1, 2, 3, 4, 5]; // Monday to Friday
+
+  return workingDays.includes(date.getDay());
+}
+
+function isWorkingHour(date) {
+  const workingHoursStart = 9;
+  const workingHoursEnd = 17;
+
+  return isWorkingDay(date) && date.getHours() >= workingHoursStart && date.getHours() < workingHoursEnd;
+}
+
+function getLocalDSTOffset() {
+  const now = new Date();
+  const standardTimeOffset = now.getTimezoneOffset(); // Get the standard time offset
+  now.setMonth(5); // Set the month to a date during DST (e.g., June)
+  const dstTimeOffset = now.getTimezoneOffset(); // Get the DST time offset
+
+  // If the standard time offset is greater than the DST time offset, DST is in effect
+  if (standardTimeOffset > dstTimeOffset) {
+    return standardTimeOffset - dstTimeOffset;
+  } else {
+    return 0; // DST is not in effect
+  }
+}
 module.exports = calculateDueDate;
